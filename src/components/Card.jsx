16b599661848f071c8db0c6fbca6e2be93e697eb
Lukas Heberling <62028575+N3moAhead@users.noreply.github.com>
2024-04-24 15:34:37 +0200

    Component: Added a Card component
    Because we will all need card components it would be good if they would
    all look the same. So i wrote a simple one that we all can use.

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View } from "react-native";
+
+export default function Card({ children }) {
+  return <View style={styles.container}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    padding: 5,
+    backgroundColor: '#FAFAFA',
+    borderRadius: 10,
+    marginTop: 10,
+  }
+});
